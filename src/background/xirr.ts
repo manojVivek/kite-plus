d0b4851e0a1499936c4e3f5c8a0dc10c1a5455e9
Manoj Vivek <p.manoj.vivek@gmail.com>
2023-12-20 17:44:26 +0530

    Cached xirr

--- src/background/xirr.ts
+++ src/background/xirr.ts
@@ -1,7 +1,10 @@
 import pLimit from 'p-limit';
 import {Instrument, XirrRequest, XirrResponse} from '../common/constants';
+import ExpiryMap from 'expiry-map';
 
 const limit = pLimit(8);
+
+const xirrCache = new ExpiryMap<string, number>(1000 * 60 * 60 * 24);
 
 const fetchXirr = async (instrumentId: string, token: string): Promise<number> => {
   try {
@@ -29,9 +32,15 @@ export const handleXirrRequest = async (request: XirrRequest): Promise<XirrRespo
   await Promise.all(
     holdings.map(async holding => {
       const instrumentId = holding.instrument_id;
+      const cachedXirr = xirrCache.get(instrumentId);
+      if (cachedXirr != null) {
+        xirrs[holding.tradingsymbol] = cachedXirr;
+        return;
+      }
       await limit(async () => {
         const xirr = await fetchXirr(instrumentId, token);
         xirrs[holding.tradingsymbol] = xirr;
+        xirrCache.set(instrumentId, xirr);
       });
     })
   );
